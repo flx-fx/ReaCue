3ee3250988389a3ef9bc1cc4936c250dc7f2bbe6
flx-fx <118358393+flx-fx@users.noreply.github.com>
2026-01-28 14:30:36 +0100

    Add Vercel Analytics integration, clean up dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { ThemeProvider } from "./components/theme-provider";
+import {ThemeProvider} from "./components/theme-provider";
 import MainConfig from "./components/main-config";
-import { Toaster } from "@/components/ui/sonner.tsx";
-import { useTheme } from "@/components/theme-provider"
+import {Toaster} from "@/components/ui/sonner.tsx";
+import {useTheme} from "@/components/theme-provider"
+import {Analytics} from "@vercel/analytics/react";
 
 export function App() {
-    const { theme } = useTheme()
+    const {theme} = useTheme()
 
     return <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <div className="flex justify-center sm:items-center items-start min-h-screen w-full gap-6">
-            <MainConfig />
+            <MainConfig/>
         </div>
-        <Toaster position="bottom-right" theme={theme} />
+        <Toaster position="bottom-right" theme={theme}/>
+        <Analytics />
     </ThemeProvider>;
 }
 
